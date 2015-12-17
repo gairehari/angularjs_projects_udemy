90771ec73c839662b20df68409d9737ee105d1ac
Hari Gaire <hariprasad.gaire@rackspace.com>
2015-12-17 17:49:21 -0600

    beautification of codes

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 var app = angular.module("computer", ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider){
+  .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/main',{
-        templateUrl: 'main.html',
-        controller: 'MainCtrl'
-      }).
-      when('/about',{
-        templateUrl: 'about.html',
-        controller: 'MainCtrl'
-      }).
-      when('/contact',{
-        templateUrl: 'contact.html',
-        controller: 'ContactCtrl'
-      }).
-      when('/services',{
-        templateUrl: 'services.html',
-        controller: 'ServicesCtrl'
-      }).
-      otherwise({
-        redirectTo: '/main'
-      });
-  }])
-
-  .controller('MainCtrl', ['$scope', '$http', function($scope, $http){
-    //console.log('This is the MainCtrl.');
-    $scope.person = 'John Doe';
-    //console.log($scope);
-    $http.get('services.json').then(function(response){
-      $scope.services = response.data;
+    when('/main', {
+      templateUrl: 'main.html',
+      controller: 'MainCtrl'
+    }).
+    when('/about', {
+      templateUrl: 'about.html',
+      controller: 'MainCtrl'
+    }).
+    when('/contact', {
+      templateUrl: 'contact.html',
+      controller: 'ContactCtrl'
+    }).
+    when('/services', {
+      templateUrl: 'services.html',
+      controller: 'ServicesCtrl'
+    }).
+    otherwise({
+      redirectTo: '/main'
     });
   }])
 
-  .controller('ContactCtrl', ['$scope', '$http', function($scope, $http){
-    console.log('This is the ContactCtrl.');
-    $http.get('locations.json').then(function(response){
-      $scope.locations = response.data;
-    });
-  }])
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+  //console.log('This is the MainCtrl.');
+  $scope.person = 'John Doe';
+  //console.log($scope);
+  $http.get('services.json').then(function(response) {
+    $scope.services = response.data;
+  });
+}])
 
-  .controller('ServicesCtrl', ['$scope', '$http', function($scope, $http){
-    console.log('This is the ServicesCtrl.');
-    $http.get('services.json').then(function(response){
-      $scope.services = response.data;
-    });
-  }]);
+.controller('ContactCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('This is the ContactCtrl.');
+  $http.get('locations.json').then(function(response) {
+    $scope.locations = response.data;
+  });
+}])
+
+.controller('ServicesCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('This is the ServicesCtrl.');
+  $http.get('services.json').then(function(response) {
+    $scope.services = response.data;
+  });
+}]);
